94d12fb63f4d0d25a77be8f78d57a6da432745c5
Ilyas Foo <foo.ilyas@gmail.com>
2020-04-03 11:07:04 +0800

    feat: added shared types and backend type transformer util

--- backend/src/utils/typeHelper.js
+++ backend/src/utils/typeHelper.js
@@ -1,15 +1,20 @@
-import { MEASURE, TRAVEL } from '../../../shared/enums';
+import { MEASURE, TRAVEL } from '../../../shared/types';
 import invert from 'lodash/invert';
 
 const MEASURE_AS_KEY = invert(MEASURE);
 const TRAVEL_AS_KEY = invert(TRAVEL);
 
-export function toMeasureEnum(value) {
+export function toMeasureType(value) {
   const key = String(value).toLowerCase();
   return MEASURE_AS_KEY[key] ? key : null;
 }
 
-export function toTravelEnum(value) {
+export function toTravelType(value) {
   const key = String(value).toLowerCase();
   return TRAVEL_AS_KEY[key] ? key : null;
+}
+
+export function toInteger(value) {
+  const int = parseInt(value);
+  return Number.isInteger(int) ? int : null;
 }
